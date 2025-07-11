7fa91407d9f1f2165272a80833cefd70a45d60a5
Bassant Ansary <64905510+Bassant77533@users.noreply.github.com>
2025-07-12 00:26:38 +0300

    Update actSearchRecipies.js

--- src/store/act/actSearchRecipies.js
+++ src/store/act/actSearchRecipies.js
@@ -6,7 +6,7 @@ const actSearchRecipes = createAsyncThunk(
   async (query, thunkAPI) => {
     const { rejectWithValue } = thunkAPI;
     try {
-      const res = await axios.get(`http://localhost:5005/recipes/search?q=${query}`);
+      const res = await axios.get(`https://fd4b7c78-2e0b-4c7a-8cb0-f1b4aea716c4-00-3sb37sgcas9gj.spock.replit.dev/recipes/search?q=${query}`);
       return res.data;
     } catch (error) {
       return rejectWithValue("Search failed");
