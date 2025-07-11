2f84f5eb4bae9fcbff64401706e0a36f0c942a6f
Bassant Ansary <64905510+Bassant77533@users.noreply.github.com>
2025-07-12 00:25:26 +0300

    Update actGetRecipies.js

--- src/store/act/actGetRecipies.js
+++ src/store/act/actGetRecipies.js
@@ -6,7 +6,7 @@ const actGetRecipies = createAsyncThunk(
   async (_, thunkAPI) => {
     const { rejectWithValue } = thunkAPI;
     try {
-      const response = await axios.get("http://localhost:5005/recipes");
+      const response = await axios.get("https://fd4b7c78-2e0b-4c7a-8cb0-f1b4aea716c4-00-3sb37sgcas9gj.spock.replit.dev/");
       return response.data;
     } catch (error) {
       if (axios.isAxiosError(error)) {
